03fcf5cf5313b5497c695af04f069f0b2759b072
dianeyacouba <dianeyacouba@gmail.com>
2021-02-18 20:18:28 +0100

    make use of Route::view instead of Route::get

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,22 @@
 
 
 
-Route::get('', function () {
-    return view('pages/home');
-})->name('home');
+// Route::get('', function () {
+//     return view('pages/home');
+// })->name('home');
 
-Route::get('about-us', function () {
-    return view('pages/about');
-})->name('about');
+// Route::get('about-us', function () {
+//     return view('pages/about');
+// })->name('about');
+
+// Route::any('/', fn()=> view('pages/home'))->name('home');
+
+// Route::any('about-us', fn()=> view('pages/about'))->name('about');
+
+Route::view('/','pages/home')->name('home');
+
+Route::view('/about-us','pages/about')->name('about');
+
+Route::get('/help', function (){
+    return view('pages.help');
+});
